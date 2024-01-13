c144d8fbc4cc159354c7db5ccaee13eecbff2d01
Valli102002 <156427829+Valli102002@users.noreply.github.com>
2024-01-13 21:19:48 +0530

    Add files via upload

---
+++ public.java
@@ -0,0 +1,9 @@
+class publi{
+ 
+public static void main(String[] agrs){
+
+System.out.println("***************");
+System.out.println("Hello World");
+System.out.println(""****************");
+}
+}
